4b97f5395cc2412e58f9c7a635a33ad9e91353a8
Ariful Islam <arifulislam161024@gmail.com>
2021-11-11 07:30:16 +0600

    get all data from db

--- index.js
+++ index.js
@@ -13,10 +13,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env
 	.DB_PASS}@firstcluster.fhu8f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // connect to mongo db
-client.connect((err) => {
-	console.log('connected to mongo db');
-});
+async function run() {
+	try {
+		await client.connect();
+		const database = client.db('dorpon');
+		const productsCollection = database.collection('products');
 
+		// get all products
+		app.get('/products', async (req, res) => {
+			const products = await productsCollection.find({}).toArray();
+			res.json(products);
+		});
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
 	res.send('Server is running');
 });
