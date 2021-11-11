90f04d6198d5aff07fe9161b7b06fb983408b32c
Ariful Islam <arifulislam161024@gmail.com>
2021-11-11 21:53:06 +0600

    save order data to db

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 // middlewear
@@ -19,16 +20,30 @@ async function run() {
 		const database = client.db('dorpon');
 		const productsCollection = database.collection('products');
 		const reviewCollection = database.collection('review');
+		const ordersCollection = database.collection('orders');
 
 		// get all products
 		app.get('/products', async (req, res) => {
 			const products = await productsCollection.find({}).toArray();
 			res.json(products);
 		});
-		// get all products
+		// get all reviews
 		app.get('/reviews', async (req, res) => {
 			const products = await reviewCollection.find({}).toArray();
 			res.json(products);
+		});
+		// get a single product by id
+		app.get('/purchase/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const product = await productsCollection.findOne(query);
+			res.json(product);
+		});
+		// save a new order
+		app.post('/orders', async (req, res) => {
+			const order = req.body;
+			const result = await ordersCollection.insertOne(order);
+			res.json(result);
 		});
 	} finally {
 		// await client.close();
